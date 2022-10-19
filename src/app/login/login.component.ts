5804ad711a8778942f6a292f8a4a6fa95813ec38
nikhilregulapati <nikhilregulapati@gmail.com>
2022-10-19 16:22:18 +0530

    made changes to the login component html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormBuilder, Validators} from '@angular/forms';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+   form: FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) {
+    this.form = this.fb.group({
+      email: ['nikhil@n.com', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required, Validators.minLength(6)]]   
+    })
+   }
 
   ngOnInit(): void {
   }
-
+  
+  login(){
+    console.log(this.form.value);
+  }
 }
